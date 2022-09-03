7d529fd18f0ada22255340863eda188c1572e971
PaulJeconiah <pauljack1672@gmail.com>
2022-09-03 15:47:56 +0800

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World1</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam eos voluptatum cum perspiciatis velit possimus quo sint. Voluptas temporibus exercitationem dolor labore fugit, asperiores aliquam commodi. Repellendus necessitatibus id illo?</p>
 </body>
 </html>
